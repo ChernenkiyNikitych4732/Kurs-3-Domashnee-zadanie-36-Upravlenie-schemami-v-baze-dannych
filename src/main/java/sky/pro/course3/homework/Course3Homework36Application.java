563583e7bdb86fe82c181a044f053ccd15d6aa1b
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-11-01 10:36:54 +0100

    Курс 3 Домашнее задание 36 Управление схемами в базе данный - изначальный комментарий

---
+++ src/main/java/sky/pro/course3/homework/Course3Homework36Application.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course3Homework36Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course3Homework36Application.class, args);
+    }
+
+}
